6b89dc84a38601e1e1976649510f05c825011739
rbous <rboussanni@gmail.com>
2024-05-14 16:52:24 -0400

    lexer char reading

--- lexer.cpp
+++ lexer.cpp
@@ -1,12 +1,67 @@
 #include <iostream>
-#include <string>
+#include <stdio.h>
 
-int main(void) {
-    return 0;
-}
+using std::string;
+using std::cout;
+using std::endl;
 
 class Lexer {
-public:
-    string source;
-    Lexer(source)
+    public:
+        char curChar;
+        int curPos;
+        string source;
+
+    public:
+        // Contructor
+        Lexer(string s) {
+            source = s + "\n";
+            curChar = ' ';
+            curPos = -1;
+            nextChar();
+        };
+
+        // Process the next character
+        void nextChar(void) {
+            curPos++;
+            if (curPos >= source.length()) {
+                curChar = '\0';
+            } else {
+                curChar = source[curPos];
+            }
+        }
+        
+        // Return the lookahead character
+        char peek(void) {
+            if (curPos + 1 >= source.length()) {
+                return '\0';
+            } 
+            return source[curPos + 1];
+        }
+
+        // Invalid token found, print error message and exit
+        void abort(void) {
+        }
+
+        // Skip whitespace except newlines
+        void skipWhitespace(void) {
+        }
+
+        // Skip comments in the code
+        void skipComment(void) {
+        }
+
+        // Return the next token
+        void getToken(void) {
+        }
 };
+
+int main(void) {
+    string source = "LET foobar = 123";
+    Lexer lexer = Lexer(source);
+
+    while (lexer.peek() != '\0') {
+        cout << lexer.curChar << endl;
+        lexer.nextChar();
+    }
+
+}
